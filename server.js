f14746d6b36fc5e6c7934e204f4535e61d9dc412
hydraulicus <852gin@gmail.com>
2025-02-13 17:16:26 +0200

    update pipeline

--- server.js
+++ server.js
@@ -1,9 +1,12 @@
-import express from 'express';
-import path from 'path';
+// import express from 'express';
+// import path from 'path';
+
+const express = require('express');
+const path = require('path');
 
 const app = express();
-const port = 3000;
-const __dirname = path.resolve(path.dirname(''));
+const port = 8080;
+// const __dirname = path.resolve(path.dirname(''));
 const fileName = 'index.html';
 app.use(express.static(path.join(__dirname, "dist")));
 // app.use(express.static("public"));
